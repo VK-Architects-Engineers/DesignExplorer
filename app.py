b1d70baf914b9e460a31b4982d2c2db4a144d024
Gertjan Van den Broek <gvandenbroek@vkgroup.be>
2023-09-18 09:42:35 +0200

    added flask

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Serve the index.html file from the 'static' directory
+    
+    return send_from_directory('', 'index.html')
+
+# This route will serve all other static files
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('', filename)
+
+if __name__ == '__main__':
+    app.run()
